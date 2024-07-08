6cbafecee9407ce99562164a760f39032f864ae6
Akash <kiraacodes@gmail.com>
2024-07-08 12:22:18 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
-
 const apiRoutes = require("./routes/index");
 
 const prepareAndStartServer = async () => {
   const app = express();
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
   app.use("/api", apiRoutes);
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
